057f98934e422a134eeb63070d299114f6cabad8
nimrodpar <nimrodpar@gmail.com>
2017-07-12 16:25:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='colored_logger',
     version='0.1',
-    py_modules=['colored_logger.py'],
+    packages=['colored_logger'],
     packages=find_packages(),
     url='https://github.com/nimrodpar/colored_logger',
     description='A colored logger',
